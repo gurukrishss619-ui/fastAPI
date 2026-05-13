8812002e08320a107d0d55f3cc773ec453ea460b
gurukrishss619-ui <gurukrishss619@gmail.com>
2026-05-13 18:50:58 +0200

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, exceptions, HTTPException, status
+from pydantic import BaseModel
+
+books = [{
+    "id":1,
+    "name":"Mein khampf"}, {
+    "id":2,
+    "name":"Ikigai"},{
+    "id":3,
+    "name":"Thirukural"},{
+    "id":4,
+    "name":"Da Vinci code"}]
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: int
+    name: str
+
+
+class BookUpdate(BaseModel):
+    name: str
+
+
+@app.get("/book")
+def get_books():
+    return books
+
+
+@app.get("/book/{id}")
+def get_book_id(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found") 
+
+@app.post("/book")
+def create_book(book: Book):
+    new_book = book.model_dump()
+    books.append(new_book)
+    return "Book successfully created"
+
+
+@app.put("/book/{id}")
+def edit_book(book_id: int, book_update: BookUpdate):
+    for book in books:
+        if book['id'] == book_id:
+            book['name'] = book_update.name
+            return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found") 
+
+
+@app.delete("/book/{id}")
+def delete_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return {"message":"Book successfully removed"}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
